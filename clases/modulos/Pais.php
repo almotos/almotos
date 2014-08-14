6d04c35ad540625330f00f9ea20664975e4f1f2b
pipe <pipeson-14@hotmail.com>
2014-08-14 10:38:25 -0500

    Modulo: Paises, Modificar integridad referencial del item eliminar y el boton eliminar varios. Cambio en la documentacion.

--- clases/modulos/Pais.php
+++ clases/modulos/Pais.php
@@ -204,14 +204,50 @@ class Pais {
      *
      */
     public function eliminar() {
-        global $sql;
+        global $sql, $textos;
 
+        //arreglo que será devuelto como respuesta
+        $respuestaEliminar = array(
+            'respuesta' => false,
+            'mensaje'   => $textos->id('ERROR_DESCONOCIDO'),
+        );
+        
         if (!isset($this->id)) {
-            return NULL;
+            return $respuestaEliminar;
         }
+         
+        //hago la validacion de la integridad referencial
+        $arreglo1 = array('estados',       'id_pais  = "'.$this->id.'"', $textos->id('ESTADOS'));//arreglo del que sale la info a consultar
+        $arreglo2 = array('articulos',     'id_pais  = "'.$this->id.'"', $textos->id('ARTICULOS'));//arreglo del que sale la info a consultar
+       
+        $arregloIntegridad  = array($arreglo1, $arreglo2);//arreglo de arreglos para realizar las consultas de integridad referencial, (ver documentacion de metodo)
+        $integridad         = Recursos::verificarIntegridad($textos->id('PAIS'), $arregloIntegridad);
 
+        /**
+         * si hay problemas con la integridad referencial, la variable integridad tiene como valor,
+         * un texto diciendo que tabla contiene n cantidad de relaciones con esta
+         */
+        if ($integridad != "") {
+            $respuestaEliminar['mensaje'] = $integridad;
+            return $respuestaEliminar;
+            
+        }
+              
+        $sql->iniciarTransaccion();
         $consulta = $sql->eliminar('paises', 'id = "'.$this->id.'"');
-        return $consulta;
+        
+        if (!($consulta)) {
+            $sql->cancelarTransaccion("Fallo en el archivo " . __FILE__ . " en la linea " .  __LINE__);
+            return $respuestaEliminar;
+            
+        } else {
+            $sql->finalizarTransaccion();
+            //todo salió bien, se envia la respuesta positiva
+            $respuestaEliminar['respuesta'] = true;
+            return $respuestaEliminar;
+            
+        }
+        
     }
 
     /**
